754b7b2cd743bb0c088a5871039157be250c9676
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-18 20:50:23 -0200

    Setup koa + stylus middleware for development.

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var koa = require('koa');
+var logger = require('koa-logger');
+var serve = require('koa-static');
+var stylus = require('koa-stylus');
+
+var app = koa();
+
+app.use(logger());
+app.use(stylus('./src'));
+app.use(serve('./src'));
+
+app.listen(process.env.PORT || 3000);
