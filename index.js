f195442729e7d2857398f352844501e7efa23739
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-09 21:45:25 -0300

    Fixed linting.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
-var koa = require('koa');
-var logger = require('koa-logger');
-var serve = require('koa-static');
-var stylus = require('koa-stylus');
+var koa = require('koa')
+var logger = require('koa-logger')
+var serve = require('koa-static')
+var stylus = require('koa-stylus')
 
-var app = koa();
+var app = koa()
 
-app.use(logger());
-app.use(stylus('./src'));
-app.use(serve('./src'));
+app.use(logger())
+app.use(stylus('./src'))
+app.use(serve('./src'))
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
