a01000c1a0b104aeea7804716dd6c5b260ebc6d4
Petherson Moreno <pethermoreno@gmail.com>
2019-12-26 23:28:43 -0300

    chore: changed e-mail to nodeschoolcps@gmail.com

--- data/config.js
+++ data/config.js
@@ -7,7 +7,7 @@ module.exports = {
   github: `nodeschool/campinas`,
   slack: ``,
   meetupGroup: `Nodeschool-Campinas`,
-  email: `pethermoreno@gmail.com`,
+  email: `nodeschoolcps@gmail.com`,
   mailchimpSubscribeUrl: `https://nodeschoolyvr.us20.list-manage.com/subscribe/post?u=703e823487e1f52bfbe0cc32d&amp;id=34f1146956`,
   credits: {
     logo: {
--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
         url: `https://nodeschool.io/campinas/`,
         slack: ``,
         meetupGroup: `Nodeschool-Campinas`,
-        email: `pethermoreno@gmail.com`,
+        email: `nodeschoolcps@gmail.com`,
         mailchimpSubscribeUrl: `https://nodeschoolyvr.us20.list-manage.com/subscribe/post?u=703e823487e1f52bfbe0cc32d&amp;id=34f1146956`,
         credits: {
           logo: {
