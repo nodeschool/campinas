98a42f1cf24f788d4f4ae9c12932f519523a8de4
Petherson Moreno <pethermoreno@gmail.com>
2020-01-05 16:04:08 -0300

    feat: added mailchimpSubscribeUrl personal to test

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
         slack: `https://nodeschoolcampinas.herokuapp.com`,
         meetupGroup: `Nodeschool-Campinas`,
         email: `nodeschoolcps@gmail.com`,
-        mailchimpSubscribeUrl: `https://nodeschoolyvr.us20.list-manage.com/subscribe/post?u=703e823487e1f52bfbe0cc32d&amp;id=34f1146956`,
+        mailchimpSubscribeUrl: `https://github.us4.list-manage.com/subscribe?u=d5e78294e4dbea6272c7a9cba&id=0b96f926a1`,
         credits: {
           logo: {
             name: `Kenneth Ormandy`,
